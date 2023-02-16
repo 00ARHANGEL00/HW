65ea740bd3bf32f2fd7433a9f56dc0665269d4d5
Max <->
2023-02-16 21:23:31 +0500

    Added Less6

---
+++ Less6/Task41/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Clear();
+
+int PositiveCol(double[] arr)
+{
+    int col = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) col++;
+    }
+    return col;
+}
+
+double[] GetArrray(int n)
+{
+    double[] res = new double[n];
+    for (int i = 0; i < n; i++)
+    {
+        res[i] = double.Parse(Console.ReadLine()!);
+    }
+    return res;
+}
+
+Console.Write("Введите сколько чисел хотите ввести: ");
+int n = int.Parse(Console.ReadLine()!);
+if (n <= 0)
+{
+    Console.WriteLine("Количество чисел не может быть отрицательным!");
+}
+else
+{
+    Console.WriteLine("Введите числа(через Enter):");
+    double[] nums = GetArrray(n);
+
+    Console.WriteLine($"Чисел больше нуля: {PositiveCol(nums)}");
+}
---
+++ Less6/Task43/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Clear();
+
+bool Validation(double[] k, double[] c)
+{
+    if (k[0] == k[1])
+    {
+        if (c[0] == c[1])
+        {
+            Console.WriteLine("Прямые лежат друг на друге, т.е. все точки совпадают между собой");
+            return false;
+        }
+        else
+        {
+            Console.WriteLine("Прямые параллельны");
+            return false;
+        }
+    }
+    return true;
+}
+
+double[] FindCrossing(double[] k, double[] c)
+{
+    double[] res = new double[2];
+    res[0] = (c[0] - c[1]) / (k[1] - k[0]);
+    res[1] = k[0] * res[0] + c[0];
+    return res;
+}
+
+double[] k = new double[2];
+double[] c = new double[2];
+
+for (int i = 0; i < 2; i++)
+{
+    Console.Write("Введите коэффициент прямой: ");
+    k[i] = double.Parse(Console.ReadLine()!);
+
+    Console.Write("Введите константу прямой: ");
+    c[i] = double.Parse(Console.ReadLine()!);
+}
+
+if (Validation(k, c)){
+    double[] cross = FindCrossing(k, c);
+    Console.WriteLine($"Точка пересечения: x={cross[0]} и y={cross[1]}");
+}
